75e91eeaab827a9c405706f943841b1ede5ea8e1
victorbaptistalemos <victorbaptistalemos@gmail.com>
2020-12-05 03:19:49 -0300

    Exercício 9 - Imprimindo, Imprimindo, Imprimindo

---
+++ livro/ex_9.py
@@ -0,0 +1,15 @@
+# Testando mais tipos de impressão
+# Aqui temos uma coisa nova estranha, lembre-se de digitá-la exatamente.
+
+days = 'Mon Tue Wed Thu Fri Sat Sun'
+months = 'Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec'
+
+print(f'Here are the days: {days}')
+print(f'Here are the months: {months}')
+
+print('''
+There's something going on here.
+With the three single-quotes(or double-quotes).
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+''')
