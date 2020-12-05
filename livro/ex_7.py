14481da07c5a3d4f82f2b56975134c1c503d6081
victorbaptistalemos <victorbaptistalemos@gmail.com>
2020-12-05 02:49:34 -0300

    Exercício 7 - Mais impressão

---
+++ livro/ex_7.py
@@ -0,0 +1,21 @@
+print('Mary has a little lamb.')
+print('It\'s fleece was white as {}.'.format('snow'))
+print('And everywhere that Mary went.')
+print('.' * 10) # what'd that do?
+
+end_1 = 'C'
+end_2 = 'h'
+end_3 = 'e'
+end_4 = 'e'
+end_5 = 's'
+end_6 = 'e'
+end_7 = 'B'
+end_8 = 'u'
+end_9 = 'r'
+end_10 = 'g'
+end_11 = 'e'
+end_12 = 'r'
+
+# Olhe a vírgula na linha seguinte, caso seja removida dará que tipo de erro?
+print(end_1 + end_2 + end_3 + end_4 + end_5 + end_6, end=' ')
+print(end_7 + end_8 + end_9 + end_10 + end_11 + end_12)
