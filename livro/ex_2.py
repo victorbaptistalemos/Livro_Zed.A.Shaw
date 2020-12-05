82996531f948ca26cd8b0429f19f502c4f359497
victorbaptistalemos <victorbaptistalemos@gmail.com>
2020-12-05 01:38:57 -0300

    Exercício 2 - Comentários e Cerquilhas

---
+++ livro/ex_2.py
@@ -0,0 +1,9 @@
+# Um comentário, assim você pode ler seu programa mais tarde
+# Qualquer coisa depois de # é ignorada pelo Python
+
+print('I could code like this.') # e o comentário é ignorado
+
+# Você também pode usar # para 'desabilitar' uma linha de código:
+# print('This won\'t run')
+
+print('This will run')
