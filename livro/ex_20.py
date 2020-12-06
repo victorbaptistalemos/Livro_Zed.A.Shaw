01bd4801320695a2b497cca79539ce0f275917eb
victorbaptistalemos <victorbaptistalemos@gmail.com>
2020-12-06 18:37:13 -0300

    Exercício 20 - Funções e Arquivos

---
+++ livro/ex_20.py
@@ -0,0 +1,27 @@
+from sys import argv
+
+script, input_file = argv
+
+def print_all(f):
+    print(f.read())
+
+def rewind(f):
+    f.seek(0)
+
+def print_a_line(line_count, f):
+    print(line_count, f.readline())
+
+current_file = open(input_file)
+
+print('First, let\'s print the whole file:\n')
+
+print_all(current_file)
+
+print('Now let\'s rewind, kind of like a tape.')
+
+rewind(current_file)
+
+print('Let\'s print four lines:\n')
+
+for current_line in range(1, 5):
+    print_a_line(current_line, current_file)
