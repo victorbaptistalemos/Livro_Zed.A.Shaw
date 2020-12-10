2a5e1958c03e3c83036d3c151f9f880037245dde
victorbaptistalemos <victorbaptistalemos@icloud.com>
2020-12-10 20:04:27 -0300

    Exercício 43 - Parte 3 (Classe Death)

--- livro/ex_43.py
+++ livro/ex_43.py
@@ -10,6 +10,20 @@ class Scene:
         exit(1)
 
 
+class Death(Scene):
+    quips = (
+        'You died. You kinda suck at this.',
+        'Your Mom would be proud...if she were smarter.',
+        'Such a luser.',
+        'I have a small puppy that\'s better at this.',
+        'You\'re worse than your Dad\'s jokes.'
+    )
+
+    def enter(self):
+        print(Death.quips[randint(0, len(self.quips) - 1)])
+        exit(1)
+
+
 if __name__ == '__main__':
-    x =  Scene()
+    x = Death()
     x.enter()
