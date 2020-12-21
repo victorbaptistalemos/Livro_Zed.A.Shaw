c2fc6839e00a38ae30aaaa742aecdba343efef1a
victorbaptistalemos <victorbaptistalemos@icloud.com>
2020-12-20 22:21:41 -0300

    Exercício 45 - Módulo perigo, Classes Abelha, Urso, Gelo, Acido, Cthulhu

---
+++ livro/ex_45_perigo.py
@@ -0,0 +1,26 @@
+from ex_45_fase import Fase
+
+
+class Abelha(Fase):
+    def __init__(self):
+        super().__init__()
+
+
+class Urso(Fase):
+    def __init__(self):
+        super().__init__()
+
+
+class Gelo(Fase):
+    def __init__(self):
+        super().__init__()
+
+
+class Acido(Fase):
+    def __init__(self):
+        super().__init__()
+
+
+class Cthulhu(Fase):
+    def __init__(self):
+        super().__init__()
